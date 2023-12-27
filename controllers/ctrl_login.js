13226af922b1fd71fe8a36f14598950ac83c2f42
cviberos <viberos993@outlook.com>
2023-12-26 22:45:46 -0300

    Mod-controller-ctrl-login1

--- controllers/ctrl_login.js
+++ controllers/ctrl_login.js
@@ -96,7 +96,7 @@ const login = async(req, res = response) => {
 const renewToken = async( req, res = response) => {
 
   const uid = req.uid;
-  const token = await generarJWT( id );
+  const token = await generarJWT( uid );
   const usuario = await Usuario.findById( uid );
 
   res.json({
